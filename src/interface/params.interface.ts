b99637b5e6fa331196971e93c863013a9a102584
Xecus <imkkai@hotmail.com>
2020-06-14 00:40:26 +0800

    允许使用字符串

--- src/interface/params.interface.ts
+++ src/interface/params.interface.ts
@@ -1,16 +1,16 @@
 export interface ITwitterApiFollowParams{
     count?: number;
-    user_id?: number;
+    user_id?: number | string;
     screen_name?: string;
-    cursor?: number;
+    cursor?: number | string;
     skip_status?: boolean;
     include_user_entities?: boolean;
 }
 
 export interface ITwitterApiTimeLineParams{
     count?: number;
-    since_id?: number;
-    max_id?: number;
+    since_id?: number | string;
+    max_id?: number | string;
     trim_user?: boolean;
     exclude_replies?: boolean;
     include_entities?: boolean;
